897f131f267a5c73a27a71956e1e68996222ae88
FaziGIT <mathis.tsrt@gmail.com>
2023-04-06 21:38:49 +0200

    test 2 réglé

--- tests/Functional/LoginPageTest.php
+++ tests/Functional/LoginPageTest.php
@@ -14,15 +14,16 @@ class LoginPageTest extends WebTestCase
         $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
         $user = $entityManager->find(User::class, 6);
         $client->loginUser($user);
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/competition/');
         // $this->assertResponseStatusCodeSame(301);
         $this->assertResponseIsSuccessful();
+        // dd($crawler);
 
 
 
         // $personnes = $crawler->filter('.d-flex');
         // $this->assertEquals(1, count($personnes));
         // $crawler->filter('h1');
-        $this->assertSelectorTextContains('h1', 'News');
+        $this->assertSelectorTextContains('h1', 'Liste des competitions');
     }
 }
