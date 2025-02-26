d2b34d54a8939d2c1524c58188d1ec518f94eefd
FaziGIT <mathis.tsrt@gmail.com>
2025-02-26 18:11:59 +0100

    fix: update user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
- * @ORM\Table(name="user")
+ * @ORM\Table(name="`user`")
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
